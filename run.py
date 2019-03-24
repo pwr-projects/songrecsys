188aedd012d2aee00b7982430a1f3fc987ca14a4
suchygrzech <suszka.grzegorz@gmail.com>
2019-03-24 04:55:08 +0100

    add playlist and songs scrapping

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from pprint import pprint
 
 from songrecsys import *
+from tqdm import tqdm
 
 if __name__ == '__main__':
     config = ConfigMgr(CONFIG_DEFAULT_PATH)
     sp = SpotifyWrapper(config)
     
-    sp.current_user_top_tracks(time_range='long_term')
+    playlist_ids = sp.get_all_playlists_of_user('spotify')  
+    songs = sp.get_tracks_from_playlists('spotify', *playlist_ids)
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     description='',
     install_requires=[
         'spotipy',
-        'tqdm'
+        'tqdm',
+        'numpy'
     ]
 )
