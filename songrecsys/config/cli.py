cfcad56f55760d6ffe04b7a657cf866a71293df9
Grzegorz <suszka.grzegorz@gmail.com>
2019-04-08 17:27:46 +0200

    fixed cli settings

--- songrecsys/config/cli.py
+++ songrecsys/config/cli.py
@@ -6,17 +6,17 @@ from .base import ConfigBase
 class ConfigCLI(ConfigBase):
     def __init__(self, *args, **kwargs):
         parser = ArgumentParser(description="Song recommendation system")
-        parser.add_argument("--spotify_id", type=str, help="Spotify app client id")
-        parser.add_argument(
-            "--spotify_secret", type=str, help="Spotify app client secret"
-        )
-        parser.add_argument("--genius_id", type=str, help="Genius app client id")
-        parser.add_argument(
-            "--genius_secret", type=str, help="Genius app client secret"
-        )
+        parser.add_argument("--spotify_id", type=str,
+                            help="Spotify app client id")
+        parser.add_argument("--spotify_secret", type=str,
+                            help="Spotify app client secret")
+        parser.add_argument("--genius_id", type=str,
+                            help="Genius app client id")
+        parser.add_argument("--genius_secret", type=str,
+                            help="Genius app client secret")
 
         args = parser.parse_args()
-        super().__init__(*args.__dict__.values())
+        super().__init__(**args.__dict__)
 
     @property
     def base_dict(self):
