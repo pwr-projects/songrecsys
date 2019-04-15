7357b92e1f526f1cd424d0f3fe0845cad54733be
Grzegorz <suszka.grzegorz@gmail.com>
2019-04-15 18:09:04 +0200

    change data dir

--- songrecsys/consts.py
+++ songrecsys/consts.py
@@ -3,10 +3,9 @@ from enum import Enum
 from os.path import join
 from typing import NewType, Text
 
-DEFAULT_PATH_CONFIG      = 'config.json'
-DEFAULT_PATH_TMP_DIR     = '.tmp'
-DEFAULT_PATH_MERGED_DATA = join(DEFAULT_PATH_TMP_DIR, 'data.json')
-DEFAULT_PATH_PLAYLISTS   = join(DEFAULT_PATH_TMP_DIR, 'playlists.json')
-DEFAULT_PATH_TRACKS      = join(DEFAULT_PATH_TMP_DIR, 'tracks.json')
-
-
+DEFAULT_PATH_CONFIG = 'config.json'
+DEFAULT_PATH_DATA_DIR = 'data'
+DEFAULT_PATH_MERGED_DATA = join(DEFAULT_PATH_DATA_DIR, 'data.json')
+DEFAULT_PATH_PLAYLISTS = join(DEFAULT_PATH_DATA_DIR, 'playlists.json')
+DEFAULT_PATH_TRACKS = join(DEFAULT_PATH_DATA_DIR, 'tracks.json')
+DEFAULT_PATH_LANG_MODELS_DIR = join(DEFAULT_PATH_DATA_DIR, 'models')
