4bf20d5b25a9ea07f632c75ff67a84fc16278a43
Grzegorz <suszka.grzegorz@gmail.com>
2019-04-15 18:09:04 +0200

    change data dir

--- songrecsys/core/nlp.py
+++ songrecsys/core/nlp.py
@@ -6,12 +6,10 @@ from pymagnitude import *
 from songrecsys.schemes import ModelPath
 
 
-class NLP:
+class NLP(Magnitude):
     def __init__(self, model_path: ModelPath):
         self._model_path = model_path
-        self._model = self._load_model()
 
-    def _load_model(self) -> Magnitude:
         model_local_path = join(dirname(__file__), '..', 'data', 'models', self._model_path)
         use_stream = not exists(model_local_path)
 
@@ -21,7 +19,5 @@ class NLP:
             self._model_path = model_local_path
             print(f'{model_local_path} will be used locally')
 
-        return Magnitude(self._model_path, stream=use_stream, log=True)
+        super().__init__(self._model_path, stream=use_stream, log=True)
 
-    def __call__(self):
-        return self._model
