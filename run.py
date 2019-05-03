3f6040f54a3057392016fb8c1b3a500e877772a5
suchygrzech <suszka.grzegorz@gmail.com>
2019-05-04 00:53:16 +0200

    update lfs

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
 
     data = load(DataFormat.pickle)
     dump(data, DataFormat.json)
-    # print(data)
 
     Summary.show(data, 4)
     # playlists, tracks = mgr.pl.get_all_playlists_and_tracks_of_user(merged_data=data.merged_data, update=False)
